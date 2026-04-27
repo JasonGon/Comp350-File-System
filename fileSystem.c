1a9d7510f53c1f0dbea5bd749d96323e7cbd3d6b
JasonGon <gonatasjg@gmail.com>
2026-04-27 09:31:52 -0400

    Changed the way that the disk is created

--- fileSystem.c
+++ fileSystem.c
@@ -10,14 +10,16 @@ int main() {
 	// The user input determines which function to run
 	char input[40];
 
-	// Set up a virtual disk. Use w+ so that the file can be written to
-	FILE *virtualDisk = fopen("disk.bin", "w+");
+	// Set up a virtual disk
+	#define DISK_BLOCKS 100	         // Disk has 100 empty slots
+	#define BLOCK_SIZE 512		 // Each disk can hold 512 bytes
+	#define RESERVED_BLOCKS 10	 // blocks 0-9 and saves them for free map region
 	printf("Formatting Disk...\n");
 
-	// Ensure the disk has 100 empty slots. Each can hold 512 bytes
-	for (int i = 0; i < 100; i++){
-		fwrite("{0}", 512, 1, virtualDisk);
-	}
+	// Define the simulated disk, the map for free space, and the file name per block
+	static char disk[DISK_BLOCKS][BLOCK_SIZE];
+	static unsigned char freeMap[DISK_BLOCKS];	// A 0 means a spot is free and 1 means full
+	static char fileTable[DISK_BLOCKS][64];		// " " means no file
 
 	printf("Disk formatted successfully. FreeMap blocks 0-9 are now allocated.\n");
 
@@ -36,9 +38,9 @@ int main() {
 		// 1. Reformats the entire disk
 		if (strcmp(input, "format\n") == 0) {
 			printf("\nReformatting disk...\n");
-			for (int i = 0; i < 100; i++){
-				fwrite("{0}", 512, 1, virtualDisk);
-			}
+			//for (int i = 0; i < 100; i++){
+			//	fwrite("{0}", 512, 1, virtualDisk);
+			//}
 			printf("\nDone!\n\n");
 		}
 
@@ -54,7 +56,7 @@ int main() {
 
 			char name[100];
 			scanf("%s", name);
-			fopen(("disk.bin\{%s}",name), "w");
+			//fopen(("disk.bin\{%s}",name), "w");
 		}
 
 
